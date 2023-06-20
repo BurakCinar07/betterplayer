0415149ee8b65e7f6e0c9853cdcd4a5bb1e727c2
BurakCinar <burakcinar07@gmail.com>
2023-06-20 13:19:59 +0300

    Fixed subtitle changed event triggering on every initiliaze instead of user interaction.

--- lib/src/core/better_player_controller.dart
+++ lib/src/core/better_player_controller.dart
@@ -362,8 +362,8 @@ class BetterPlayerController {
       subtitlesLines.addAll(subtitlesParsed);
     }
 
-    _postEvent(BetterPlayerEvent(BetterPlayerEventType.changedSubtitles));
     if (!_disposed && !sourceInitialize) {
+      _postEvent(BetterPlayerEvent(BetterPlayerEventType.changedSubtitles));
       _postControllerEvent(BetterPlayerControllerEvent.changeSubtitles);
     }
   }
